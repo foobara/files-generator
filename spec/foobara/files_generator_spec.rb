d6f8729390d3fdc55e4e72917b4592dec0b8dbce
Miles Georgi <azimux@gmail.com>
2024-02-19 14:02:37 -0800

    Initial commit, gem generated in RubyMine

---
+++ spec/foobara/files_generator_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Foobara::FilesGenerator do
+  it "has a version number" do
+    expect(Foobara::FilesGenerator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
